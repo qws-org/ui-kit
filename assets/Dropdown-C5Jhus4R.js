68743a62dc11338b264c782e1d086b3a5811c819
andrey-glotov <andrey-glotov@users.noreply.github.com>
2025-05-27 04:37:53 +0000

    deploy: 2922b485ded5f95965f353371be496d2d74b4712

---
+++ assets/Dropdown-C5Jhus4R.js
@@ -0,0 +1,201 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as i}from"./index-B8rYRX2K.js";import{M as p,C as d,a as o}from"./index-DPPDblrR.js";import{s as m,D as s,S as l,H as c,a,W as w,L as D,C as g,b as x}from"./Dropdown.stories-BRL3Q0cR.js";import"./index-D4lIrffr.js";import"./iframe-Td6y2OXS.js";import"./index-CLq2iIfz.js";import"./index-DQD4w61r.js";import"./index-DsJinFGm.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Grid-WrwMUml9.js";import"./index-DW2zCSF8.js";import"./Box-DAfS7HdX.js";import"./context-CsAOKUm5.js";import"./index-HUzi1mn4.js";import"./UserCircle-Blv_RV81.js";import"./Flex-4S3tVefv.js";import"./mergeProps-CASEEftK.js";import"./useUiKitComponents-t3zy5Zo0.js";import"./filterDOMProps-CeZl_uWj.js";import"./useSelectableList-GvURuuA5.js";import"./DOMLayoutDelegate-h8l8aDcK.js";import"./useControlledState-YrJM5BK_.js";import"./platform-DzkwHEFt.js";import"./useLocalizedStringFormatter-Da5qmNid.js";import"./useFocusable-cChvM0Y4.js";import"./Overlay-Dtg55f1a.js";import"./useFocusWithin-Yfmk6-fR.js";import"./domHelpers-DlYETLWZ.js";import"./Popover-DjGcZcoV.js";import"./VisuallyHidden-SZFRcEgu.js";import"./useButton-Bw-pT_gu.js";function t(r){const e={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...i(),...r.components};return n.jsxs(n.Fragment,{children:[n.jsx(p,{of:m}),`
+`,n.jsx(e.h1,{id:"dropdown",children:"Dropdown"}),`
+`,n.jsx(e.p,{children:"Компонент для группировки элементов в выпадающем меню"}),`
+`,n.jsx(d,{}),`
+`,n.jsx(e.h2,{id:"default-code-example",children:"Default code example"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { Dropdown } from "./Dropdown";
+
+<Dropdown.Trigger
+  strategy="click"
+  onAction={(key) => alert(key)}
+  triggerElement={<button>Click to Open</button>}
+>
+  <Dropdown.Item key="adobe">Adobe</Dropdown.Item>
+  <Dropdown.Item key="apple">Apple</Dropdown.Item>
+  <Dropdown.Item key="google">Google</Dropdown.Item>
+  <Dropdown.Item key="microsoft">Microsoft</Dropdown.Item>
+</Dropdown.Trigger>;
+`})}),`
+`,n.jsx(o,{of:s}),`
+`,n.jsx(e.h2,{id:"section-code-example",children:"Section code example"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { Dropdown } from "./Dropdown";
+
+<Dropdown.Trigger
+  strategy="click"
+  onAction={(key) => alert(key)}
+  triggerElement={<button>Click to Open</button>}
+  cols={2}
+>
+  <Dropdown.Section title="Styles">
+    <Dropdown.Item key="bold">Bold</Dropdown.Item>
+    <Dropdown.Item key="underline">Underline</Dropdown.Item>
+  </Dropdown.Section>
+  <Dropdown.Section title="Align">
+    <Dropdown.Item key="left">Left</Dropdown.Item>
+    <Dropdown.Item key="middle">Middle</Dropdown.Item>
+    <Dropdown.Item key="right">Right</Dropdown.Item>
+  </Dropdown.Section>
+</Dropdown.Trigger>;
+`})}),`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(e.h2,{id:"hover-strategy-code-example",children:"Hover strategy code example"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { Dropdown } from "./Dropdown";
+import { Grid } from "./Grid";
+
+<Dropdown.Trigger
+  strategy="hover"
+  onAction={(key) => alert(key)}
+  triggerElement={<div>Hover to open</div>}
+  dropdownWrapper={(wrapperProps) => (
+    <Grid gap={8} cols={2}>
+      {wrapperProps.children}
+    </Grid>
+  )}
+>
+  <Dropdown.Section title="Styles">
+    <Dropdown.Item key="italic">Italic</Dropdown.Item>
+    <Dropdown.Item key="strike">Strikethrough</Dropdown.Item>
+  </Dropdown.Section>
+  <Dropdown.Section title="Alignment">
+    <Dropdown.Item key="justify">Justify</Dropdown.Item>
+    <Dropdown.Item key="left">Left</Dropdown.Item>
+    <Dropdown.Item key="right">Right</Dropdown.Item>
+  </Dropdown.Section>
+</Dropdown.Trigger>;
+`})}),`
+`,n.jsx(o,{of:c}),`
+`,n.jsx(e.h2,{id:"dynamic-items-code-example",children:"Dynamic items code example"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { Dropdown } from "./Dropdown";
+import { Grid } from "./Grid";
+
+const openWindows = [
+  {
+    name: "Left Panel",
+    id: "left",
+    children: [{ id: 1, name: "Final Copy (1)" }],
+  },
+  {
+    name: "Right Panel",
+    id: "right",
+    children: [
+      { id: 2, name: "index.tsx" },
+      { id: 3, name: "package.json" },
+      { id: 4, name: "license.txt" },
+    ],
+  },
+];
+
+return (
+  <Dropdown.Trigger
+    strategy="click"
+    onAction={(key) => alert(key)}
+    triggerElement={<div>Click me</div>}
+    dropdownWrapper={(wrapperProps) => (
+      <Grid gap={8} cols={2}>
+        {wrapperProps.children}
+      </Grid>
+    )}
+    items={openWindows}
+  >
+    {(item) => (
+      <Dropdown.Section items={item.children} title={item.name}>
+        {(sectionItem) => <Dropdown.Item>{sectionItem.name}</Dropdown.Item>}
+      </Dropdown.Section>
+    )}
+  </Dropdown.Trigger>
+);
+`})}),`
+`,n.jsx(o,{of:a}),`
+`,n.jsx(e.h2,{id:"items-with-icon-code-example",children:"Items with icon code example"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { Dropdown } from "./Dropdown";
+import { Flex } from "./Flex";
+import { UserCircle } from "~/icons";
+
+<Dropdown.Trigger
+  triggerElement={<div>Click to open</div>}
+  onAction={alert}
+  disabledKeys={["paste"]}
+>
+  <Dropdown.Item key="copy">
+    <Flex align="center" gap={16}>
+      <UserCircle viewBox="0 0 32 32" className="w-6 h-6" />
+      <div>Cope</div>
+    </Flex>
+  </Dropdown.Item>
+  <Dropdown.Item key="cut">
+    <Flex align="center" gap={16}>
+      <UserCircle viewBox="0 0 32 32" className="w-6 h-6" />
+      <div>Cut</div>
+    </Flex>
+  </Dropdown.Item>
+  <Dropdown.Item key="paste">
+    <Flex align="center" gap={16}>
+      <UserCircle viewBox="0 0 32 32" className="w-6 h-6" />
+      <div>Paste</div>
+    </Flex>
+  </Dropdown.Item>
+</Dropdown.Trigger>;
+`})}),`
+`,n.jsx(o,{of:w}),`
+`,n.jsx(e.h2,{id:"link-items-code-example",children:"Link items code example"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { Dropdown } from "./Dropdown";
+
+<Dropdown.Trigger triggerElement={<button>Click to open</button>}>
+  <Dropdown.Item href="https://adobe.com/" target="_blank">
+    Adobe
+  </Dropdown.Item>
+  <Dropdown.Item href="https://apple.com/" target="_blank">
+    Apple
+  </Dropdown.Item>
+  <Dropdown.Item href="https://google.com/" target="_blank">
+    Google
+  </Dropdown.Item>
+  <Dropdown.Item href="https://microsoft.com/" target="_blank">
+    Microsoft
+  </Dropdown.Item>
+</Dropdown.Trigger>;
+`})}),`
+`,n.jsx(o,{of:D}),`
+`,n.jsx(e.h2,{id:"custom-wrapper-code-example",children:"Custom wrapper code example"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { Dropdown } from "./Dropdown";
+import { Grid } from "./Grid";
+
+<Dropdown.Trigger
+  strategy="hover"
+  onAction={(key) => alert(key)}
+  triggerElement={<div>Hover to see</div>}
+  dropdownWrapper={(wrapperProps) => (
+    <Grid gap={8} cols={2}>
+      {wrapperProps.children}
+    </Grid>
+  )}
+>
+  <Dropdown.Section title="Styles">
+    <Dropdown.Item key="italic">Italic</Dropdown.Item>
+    <Dropdown.Item key="strike">Strikethrough</Dropdown.Item>
+  </Dropdown.Section>
+  <Dropdown.Section title="Alignment">
+    <Dropdown.Item key="justify">Justify</Dropdown.Item>
+    <Dropdown.Item key="left">Left</Dropdown.Item>
+    <Dropdown.Item key="right">Right</Dropdown.Item>
+  </Dropdown.Section>
+</Dropdown.Trigger>;
+`})}),`
+`,n.jsx(o,{of:g}),`
+`,n.jsx(e.h2,{id:"custom-offset-code-example",children:"Custom offset code example"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-tsx",children:`import { Dropdown } from "./Dropdown";
+
+<Dropdown.Trigger
+  strategy="hover"
+  offset={30}
+  onAction={(key) => alert(key)}
+  triggerElement={<div>Hover to see</div>}
+>
+  <Dropdown.Section title="Alignment">
+    <Dropdown.Item key="justify">Justify</Dropdown.Item>
+    <Dropdown.Item key="left">Left</Dropdown.Item>
+    <Dropdown.Item key="right">Right</Dropdown.Item>
+  </Dropdown.Section>
+</Dropdown.Trigger>;
+`})}),`
+`,n.jsx(o,{of:x})]})}function V(r={}){const{wrapper:e}={...i(),...r.components};return e?n.jsx(e,{...r,children:n.jsx(t,{...r})}):t(r)}export{V as default};
---
+++ assets/Pagination-BYFt12xy.js
@@ -0,0 +1,29 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as r}from"./index-B8rYRX2K.js";import{M as a,C as i,a as m}from"./index-DPPDblrR.js";import{s,D as p}from"./Pagination.stories-BxSSDT9P.js";import"./index-D4lIrffr.js";import"./iframe-Td6y2OXS.js";import"./index-CLq2iIfz.js";import"./index-DQD4w61r.js";import"./index-DsJinFGm.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Container-Cr9IWE7t.js";import"./Flex-4S3tVefv.js";import"./Box-DAfS7HdX.js";import"./context-CsAOKUm5.js";import"./index-HUzi1mn4.js";import"./Button-DLqEWYsL.js";import"./useButton-Bw-pT_gu.js";import"./mergeProps-CASEEftK.js";import"./useUiKitComponents-t3zy5Zo0.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-cChvM0Y4.js";import"./index-DW2zCSF8.js";function e(o){const t={code:"code",h1:"h1",h3:"h3",p:"p",pre:"pre",...r(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:s}),`
+`,n.jsx(t.h1,{id:"pagination-component",children:"Pagination Component"}),`
+`,n.jsx(t.p,{children:"Компонент для отображения пагинации. Он позволяет пользователям переключаться между страницами и отображать количество элементов на каждой странице."}),`
+`,n.jsx(i,{}),`
+`,n.jsx(t.h3,{id:"default-code-example",children:"Default code example"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`import { useState } from "react";
+import { Pagination } from "./Pagination";
+import { Container } from "./Container";
+
+const Page = () => {
+  const [page, setPage] = useState(0);
+  const content = \`Контент для страницы \${page + 1}\`;
+
+  return (
+    <>
+      <Pagination
+        totalPages={5}
+        currentPage={page}
+        onPageChange={setPage}
+        itemsPerPage={10}
+      />
+      <Container justify="center" className="mt-20">
+        {content}
+      </Container>
+    </>
+  );
+};
+`})}),`
+`,n.jsx(m,{of:p})]})}function R(o={}){const{wrapper:t}={...r(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(e,{...o})}):e(o)}export{R as default};
