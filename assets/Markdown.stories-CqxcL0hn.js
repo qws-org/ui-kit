d2a5db456fc4b10c8a808fa353c0d695f5b49135
arslanBekbolotov <arslanBekbolotov@users.noreply.github.com>
2025-06-04 10:21:07 +0000

    deploy: d06e3d3a5c8c1f42698187c786d75ebad3614428

---
+++ assets/Markdown.stories-CqxcL0hn.js
@@ -0,0 +1,66 @@
+import{M as y}from"./Markdown-B-Luf5tb.js";const M={title:"UI-Kit typography components/Markdown",component:y,args:{children:`# Заголовок 1
+
+Текст **жирный**, *курсив*, ~~зачеркнутый~~.`,className:"",alignment:"left"},argTypes:{children:{control:"text",description:"Markdown-контент для рендеринга",table:{category:"Контент"}},className:{control:"text",description:"Дополнительный CSS-класс",table:{category:"Стилизация"}},alignment:{control:{type:"select",options:["left","center","right","justify"]},description:"Выравнивание текста внутри Markdown-блока.",table:{category:"Стилизация"}}}},e={},r={args:{children:`
+# 📌 Пример сложного Markdown  
+| Имя      | Возраст | Город     |  
+|----------|--------|-----------|  
+| Иван     | 25     | Москва    |  
+| Ольга    | 30     | Санкт-Петербург |  
+
+\`\`\`tsx
+const hello = "Hello, world!";
+console.log(hello);
+\`\`\`
+
+![Логотип](/public/promo-card.png)
+> _Цитата_: "Markdown — это просто и удобно!"
+    `}},n={args:{children:"<h1 style='font-size: 40px'>Заголовок H1</h1><p style='color: red'>Красный текст внутри HTML</p>"}},o={args:{p:32,m:32,d:{p:{px:40,pb:16},m:{my:96}},children:`
+### 🌟 Кастомные стили  
+- Это список  
+- Стили применяются через className  
+- Можно настроить разную типографику  
+    `,className:"text-purple-400",alignment:"center"}};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var c,l,p;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: \`
+# 📌 Пример сложного Markdown  
+| Имя      | Возраст | Город     |  
+|----------|--------|-----------|  
+| Иван     | 25     | Москва    |  
+| Ольга    | 30     | Санкт-Петербург |  
+
+\\\`\\\`\\\`tsx
+const hello = "Hello, world!";
+console.log(hello);
+\\\`\\\`\\\`
+
+![Логотип](/public/promo-card.png)
+> _Цитата_: "Markdown — это просто и удобно!"
+    \`
+  }
+}`,...(p=(l=r.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var d,m,i;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    children: "<h1 style='font-size: 40px'>Заголовок H1</h1><p style='color: red'>Красный текст внутри HTML</p>"
+  }
+}`,...(i=(m=n.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};var g,u,h;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    p: 32,
+    m: 32,
+    d: {
+      p: {
+        px: 40,
+        pb: 16
+      },
+      m: {
+        my: 96
+      }
+    },
+    children: \`
+### 🌟 Кастомные стили  
+- Это список  
+- Стили применяются через className  
+- Можно настроить разную типографику  
+    \`,
+    className: "text-purple-400",
+    alignment: "center"
+  }
+}`,...(h=(u=o.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};const w=["Default","AdvancedMarkdown","WithHTML","CustomStyled"],f=Object.freeze(Object.defineProperty({__proto__:null,AdvancedMarkdown:r,CustomStyled:o,Default:e,WithHTML:n,__namedExportsOrder:w,default:M},Symbol.toStringTag,{value:"Module"}));export{r as A,o as C,e as D,n as W,f as s};
---
+++ assets/Modal.stories-BhjYkRSW.js
@@ -0,0 +1,42 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{B as _}from"./Button-CxkxwWo1.js";import{c as v,d as T,b as C,e as O,f as w,$ as F,a as P}from"./Overlay-Czkf0Q-5.js";import{r as D,R as k}from"./index-D4lIrffr.js";import{$ as E}from"./mergeProps-DUrzxDUC.js";import"./useButton-CYvprEwE.js";import"./filterDOMProps-CeZl_uWj.js";import"./useFocusable-D8DZ0gut.js";import"./useUiKitComponents-C9IiVWkE.js";import"./context-CsAOKUm5.js";import"./index-DQD4w61r.js";import"./index-DsJinFGm.js";import"./index-DW2zCSF8.js";import"./useUiKitTheme-C4kvSEf-.js";import"./useControlledState-YrJM5BK_.js";import"./useFocusWithin-yXpf04FA.js";import"./platform-B5sWMkIr.js";import"./domHelpers-DlYETLWZ.js";const S=s=>e.jsx("svg",{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,fill:"none",...s,children:e.jsx("path",{fill:"#fff",fillRule:"evenodd",d:"M5.637 4.223a1 1 0 0 0-1.414 1.414L10.587 12l-6.364 6.364a1 1 0 1 0 1.414 1.414L12 13.415l6.364 6.364a1 1 0 1 0 1.414-1.414L13.415 12l6.364-6.364a1 1 0 0 0-1.414-1.414L12 10.587z",clipRule:"evenodd"})});try{Close.displayName="Close",Close.__docgenInfo={description:"",displayName:"Close",props:{}}}catch{}function L(s,o,a){let{overlayProps:l,underlayProps:r}=v({...s,isOpen:o.isOpen,onClose:o.close},a);return T({isDisabled:!o.isOpen}),C(),D.useEffect(()=>{if(o.isOpen&&a.current)return O([a.current])},[o.isOpen,a]),{modalProps:E(l),underlayProps:r}}const m=({state:s,children:o,...a})=>{const l=D.useRef(null),{modalProps:r,underlayProps:c}=L(a,s,l);return e.jsx(w,{children:e.jsxs("div",{className:"fixed inset-0 z-50 bg-[rgba(22,20,25,0.9)] flex items-center justify-center",...c,children:[a.isDismissable&&e.jsx(S,{onClick:()=>{s.close()},className:"absolute top-3.5 right-4 md:hidden"}),e.jsx("div",{...r,ref:l,className:"rounded-lg",children:o})]})})};try{m.displayName="Modal",m.__docgenInfo={description:"",displayName:"Modal",props:{state:{defaultValue:null,description:"",name:"state",required:!0,type:{name:"OverlayTriggerState"}}}}}catch{}const n=({label:s,children:o,...a})=>{const l=F(a),{triggerProps:r,overlayProps:c}=P({type:"dialog"},l);return e.jsxs(e.Fragment,{children:[e.jsx(_,{...a,...r,onPress:p=>{var b,u;(b=a==null?void 0:a.onPress)==null||b.call(a,p),(u=r==null?void 0:r.onPress)==null||u.call(r,p)},children:s}),l.isOpen&&e.jsx(m,{...a,state:l,children:k.cloneElement(o(()=>l.close()),c)})]})};try{n.displayName="ModalTrigger",n.__docgenInfo={description:"",displayName:"ModalTrigger",props:{label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string"}}}}}catch{}const se={title:"UI-Kit components/Modal",component:n,parameters:{docs:{description:{component:`Компонент ModalTrigger является кнопкой, которая открывает модалку при клике. 
+Внешний вид и поведение модалки настраиваются через пропсы, такие как isDismissable (для закрытия модалки по клику вне ее) и другие. 
+Этот компонент реализует доступный способ отображения модальных окон с использованием библиотеки @react-aria.`}}},argTypes:{label:{control:"text",description:"Текст, отображаемый на кнопке, которая открывает модалку."},isDismissable:{control:"boolean",description:"Позволяет ли модалке быть закрытой по клику вне ее или по нажатию на клавишу Escape."}}},t={render:()=>e.jsx(n,{label:"Open default Modal",isDismissable:!0,children:s=>e.jsxs("div",{className:"p-4",children:[e.jsx("h2",{children:"Default Modal"}),e.jsx("p",{children:"This is a basic modal with simple text content."}),e.jsx("button",{onClick:s,className:"mt-4 bg-blue-500 text-white p-2 rounded",children:"Close Modal"})]})})},i={render:()=>e.jsx(n,{label:"Open Form Modal",isDismissable:!0,children:s=>e.jsxs("form",{className:"flex flex-col",children:[e.jsx("label",{htmlFor:"first-name",className:"mb-2",children:"First Name:"}),e.jsx("input",{id:"first-name",placeholder:"John",className:"p-2 border border-gray-300 rounded-md"}),e.jsx("label",{htmlFor:"last-name",className:"mt-4 mb-2",children:"Last Name:"}),e.jsx("input",{id:"last-name",placeholder:"Doe",className:"p-2 border border-gray-300 rounded-md"}),e.jsx(_,{className:"mt-4",onPress:s,children:"Submit"})]})})},d={render:()=>e.jsx(n,{label:"Non-Dismissable Modal",children:s=>e.jsxs("div",{className:"p-4",children:[e.jsx("h2",{children:"Non-Dismissible Modal"}),e.jsx("p",{children:"This modal cannot be closed by clicking outside or pressing the Escape key."}),e.jsx("button",{onClick:s,className:"mt-4 bg-blue-500 text-white p-2 rounded",children:"Close Modal"})]})})};var f,h,x;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: () => <ModalTrigger label="Open default Modal" isDismissable>
+      {close => <div className="p-4">
+          <h2>Default Modal</h2>
+          <p>This is a basic modal with simple text content.</p>
+          <button onClick={close} className="mt-4 bg-blue-500 text-white p-2 rounded">
+            Close Modal
+          </button>
+        </div>}
+    </ModalTrigger>
+}`,...(x=(h=t.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};var g,N,y;i.parameters={...i.parameters,docs:{...(g=i.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: () => <ModalTrigger label="Open Form Modal" isDismissable>
+      {close => <form className="flex flex-col">
+          <label htmlFor="first-name" className="mb-2">
+            First Name:
+          </label>
+          <input id="first-name" placeholder="John" className="p-2 border border-gray-300 rounded-md" />
+          <label htmlFor="last-name" className="mt-4 mb-2">
+            Last Name:
+          </label>
+          <input id="last-name" placeholder="Doe" className="p-2 border border-gray-300 rounded-md" />
+          <Button className="mt-4" onPress={close}>
+            Submit
+          </Button>
+        </form>}
+    </ModalTrigger>
+}`,...(y=(N=i.parameters)==null?void 0:N.docs)==null?void 0:y.source}}};var M,j,$;d.parameters={...d.parameters,docs:{...(M=d.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: () => <ModalTrigger label="Non-Dismissable Modal">
+      {close => <div className="p-4">
+          <h2>Non-Dismissible Modal</h2>
+          <p>
+            This modal cannot be closed by clicking outside or pressing the
+            Escape key.
+          </p>
+          <button onClick={close} className="mt-4 bg-blue-500 text-white p-2 rounded">
+            Close Modal
+          </button>
+        </div>}
+    </ModalTrigger>
+}`,...($=(j=d.parameters)==null?void 0:j.docs)==null?void 0:$.source}}};const ae=["Default","FormModal","NonDismissibleModal"];export{t as Default,i as FormModal,d as NonDismissibleModal,ae as __namedExportsOrder,se as default};
