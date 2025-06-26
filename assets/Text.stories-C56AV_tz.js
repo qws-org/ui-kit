412c7e0f90720ba5335dbf0890df2282673529e4
arslanBekbolotov <arslanBekbolotov@users.noreply.github.com>
2025-06-26 12:42:33 +0000

    deploy: fc2b99d272c55f33b6c79c802cb466c8f075d2f9

---
+++ assets/Text.stories-C56AV_tz.js
@@ -0,0 +1,103 @@
+import{T}from"./Text-VGgf03Dn.js";import"./jsx-runtime-D_zvdyIk.js";import"./Markdown-jhFRyyv0.js";import"./index-DW2zCSF8.js";import"./Box-DJYY4l9F.js";import"./index-D4lIrffr.js";import"./useUiKitTheme-DryOdeWV.js";import"./context-CsAOKUm5.js";const j={title:"UI-Kit typography components/Text",component:T,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},e={args:{m:12,p:12,content:`## 1. Introduction
+1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.
+         
+1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.
+
+## 2. Your Account
+### 2.1 Legal requirements
+2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+
+2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France
+
+![welcome-offer-bg](https://jpws-cms-dev-test-test.jack-flush.com/assets/dd95fd77-37b6-418b-a497-bf550a87124f.PNG?width=300&amp)
+## 3. Multi Accounts
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+ On your Android device, open Chrome
+
+ 1. Tap the ⋮ (three dots) icon to the right of the address bar
+
+ 2. Select Clear browsing data
+
+ 3. In the Basic settings section:
+
+ 4. Choose a time range (default is 15 minutes)
+
+ 5. Tap Clear data `}},n={args:{size:"large",content:`## 1. Introduction
+1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.
+         
+1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.
+
+## 2. Your Account
+### 2.1 Legal requirements
+2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+
+2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France
+
+![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)
+## 3. Multi Accounts
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+`}},t={args:{size:"medium",content:`## 1. Introduction
+1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.
+         
+1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.
+
+## 2. Your Account
+### 2.1 Legal requirements
+2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+
+2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France
+
+![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)
+## 3. Multi Accounts
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+`}},o={args:{size:"small",content:`## 1. Introduction
+1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.
+         
+1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.
+
+## 2. Your Account
+### 2.1 Legal requirements
+2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+
+2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France
+
+![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)
+## 3. Multi Accounts
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+`}},a={args:{size:"large",md:{display:"hidden"},d:{display:"block"},content:"Content is hidden in tablet screen"}};var r,s,c;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    m: 12,
+    p: 12,
+    content: "## 1. Introduction\\n1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.\\n         \\n1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.\\n\\n## 2. Your Account\\n### 2.1 Legal requirements\\n2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n\\n2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France\\n\\n![welcome-offer-bg](https://jpws-cms-dev-test-test.jack-flush.com/assets/dd95fd77-37b6-418b-a497-bf550a87124f.PNG?width=300&amp)\\n## 3. Multi Accounts\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n On your Android device, open Chrome\\n\\n 1. Tap the ⋮ (three dots) icon to the right of the address bar\\n\\n 2. Select Clear browsing data\\n\\n 3. In the Basic settings section:\\n\\n 4. Choose a time range (default is 15 minutes)\\n\\n 5. Tap Clear data "
+  }
+}`,...(c=(s=e.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};var i,u,d;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    content: "## 1. Introduction\\n1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.\\n         \\n1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.\\n\\n## 2. Your Account\\n### 2.1 Legal requirements\\n2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n\\n2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France\\n\\n![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)\\n## 3. Multi Accounts\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n"
+  }
+}`,...(d=(u=n.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var g,h,l;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: "medium",
+    content: "## 1. Introduction\\n1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.\\n         \\n1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.\\n\\n## 2. Your Account\\n### 2.1 Legal requirements\\n2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n\\n2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France\\n\\n![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)\\n## 3. Multi Accounts\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n"
+  }
+}`,...(l=(h=t.parameters)==null?void 0:h.docs)==null?void 0:l.source}}};var m,b,f;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    content: "## 1. Introduction\\n1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.\\n         \\n1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.\\n\\n## 2. Your Account\\n### 2.1 Legal requirements\\n2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n\\n2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France\\n\\n![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)\\n## 3. Multi Accounts\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n"
+  }
+}`,...(f=(b=o.parameters)==null?void 0:b.docs)==null?void 0:f.source}}};var y,p,A;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    md: {
+      display: "hidden"
+    },
+    d: {
+      display: "block"
+    },
+    content: "Content is hidden in tablet screen"
+  }
+}`,...(A=(p=a.parameters)==null?void 0:p.docs)==null?void 0:A.source}}};const B=["Primary","Large","Medium","Small","DisplayHidden"];export{a as DisplayHidden,n as Large,t as Medium,e as Primary,o as Small,B as __namedExportsOrder,j as default};
