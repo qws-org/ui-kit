17aa32317a3a9ede86a2f5cf23aca6900743f9d4
Arslan Bekbolotov <arslanbekbolotov58@gmail.com>
2025-07-22 11:13:46 +0600

    fix: [EV-7599] добавлен пропc imageClassName для передачи стилей напрямую на тег <img>

--- src/components/image/index.tsx
+++ src/components/image/index.tsx
@@ -8,17 +8,29 @@ export type ImageProps = {
   mdValue?: string;
   dValue?: string;
   className?: string;
+  imageClassName?: string;
 } & Partial<BoxProps>;
 
 export const Image: FC<ImageProps> = (props) => {
-  const { value, mdValue, dValue, className = "", ...rest } = props;
+  const {
+    value,
+    mdValue,
+    dValue,
+    className = "",
+    imageClassName,
+    ...rest
+  } = props;
 
   return (
     <Box className={`${className ?? ""}`} {...rest} as={"picture"}>
       <source media="(min-width: 1024px)" srcSet={dValue} type="image/webp" />
       <source media="(min-width: 768px)" srcSet={mdValue} type="image/webp" />
 
-      <img className="w-full" alt="image" src={value} />
+      <img
+        className={`w-full ${imageClassName ?? ""}`}
+        alt="image"
+        src={value}
+      />
     </Box>
   );
 };
