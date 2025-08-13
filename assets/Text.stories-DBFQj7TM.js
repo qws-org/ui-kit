0208716e24f1a37573e0bb0d1e0e6fd3022de034
alekseishmidko <alekseishmidko@users.noreply.github.com>
2025-08-13 07:58:19 +0000

    deploy: 554b4552122a8d33670ceac283a04b062cc26885

---
+++ assets/Text.stories-DBFQj7TM.js
@@ -0,0 +1,143 @@
+import{T as k}from"./Text-D6qtLNcG.js";import"./jsx-runtime-D_zvdyIk.js";import"./Markdown-Ca2o9xyK.js";import"./index-DW2zCSF8.js";import"./Box-Db_zQf3q.js";import"./index-D4lIrffr.js";import"./useUiKitTheme-CDzDdjzd.js";import"./context-CsAOKUm5.js";const F={title:"UI-Kit typography components/Text",component:k,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},e={args:{m:12,p:12,content:`## 1. Introduction
+1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.
+         
+1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.
+
+## 2. Your Account
+### 2.1 Legal requirements
+2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+
+2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France
+
+![welcome-offer-bg](https://jpws-cms-dev-test-test.jack-flush.com/assets/dd95fd77-37b6-418b-a497-bf550a87124f.PNG?width=300&amp)
+## 3. Multi Accounts
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+ On your Android device, open Chrome
+
+ 1. Tap the ⋮ (three dots) icon to the right of the address bar
+
+ 2. Select Clear browsing data
+
+ 3. In the Basic settings section:
+
+ 4. Choose a time range (default is 15 minutes)
+
+ 5. Tap Clear data `}},t={args:{size:"large",content:`## 1. Introduction
+1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.
+         
+1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.
+
+## 2. Your Account
+### 2.1 Legal requirements
+2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+
+2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France
+
+![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)
+## 3. Multi Accounts
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+`}},n={args:{size:"medium",content:`## 1. Introduction
+1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.
+         
+1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.
+
+## 2. Your Account
+### 2.1 Legal requirements
+2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+
+2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France
+
+![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)
+## 3. Multi Accounts
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+`}},o={args:{size:"small",content:`## 1. Introduction
+1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.
+         
+1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.
+
+## 2. Your Account
+### 2.1 Legal requirements
+2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+
+2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France
+
+![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)
+## 3. Multi Accounts
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.
+`}},a={args:{size:"large",md:{display:"hidden"},d:{display:"block"},content:"Content is hidden in tablet screen"}},r={args:{content:`<h2 class="info-block-title">Understanding the Betting Actions in 5 Card Omaha</h2>
+<div class="info-block-description">
+<p>Getting to grips with betting actions is essential to playing poker games well. The main betting choices offered in most poker variations are summarised as follows:</p>
+<table>
+<thead>
+<tr>
+<th>Betting Option</th>
+<th>Usage</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Check</td>
+<td>If there isn&rsquo;t a bet in the current betting round, we can use this option to move the action to our left without placing a wager.</td>
+</tr>
+<tr>
+<td>Bet</td>
+<td>We placed the first wager in this round of betting, compelling other players to either match it or fold.</td>
+</tr>
+<tr>
+<td>Fold</td>
+<td>We refuse to match the bet that is currently in play. Resigning from the pot and forfeiting any claim to it is known as folding.</td>
+</tr>
+<tr>
+<td>Call</td>
+<td>If there is an existing wager in the current betting round, &ldquo;calling&rdquo; means precisely matching that stake to remain in the hand.</td>
+</tr>
+<tr>
+<td>Raise</td>
+<td>We have the option of raising any wager that is already in place during the current betting round. The first bettor is then required to fold or match our raise.</td>
+</tr>
+<tr>
+<td>Re-raise</td>
+<td>We have the option of raising again if a player has already raised on the current street. &ldquo;Re-raises,&rdquo; with names like &ldquo;3-bets,&rdquo; &ldquo;4-bets,&rdquo; and so on, are raises that come after the initial one.</td>
+</tr>
+</tbody>
+</table>`}};var s,i,c;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    m: 12,
+    p: 12,
+    content: "## 1. Introduction\\n1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.\\n         \\n1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.\\n\\n## 2. Your Account\\n### 2.1 Legal requirements\\n2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n\\n2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France\\n\\n![welcome-offer-bg](https://jpws-cms-dev-test-test.jack-flush.com/assets/dd95fd77-37b6-418b-a497-bf550a87124f.PNG?width=300&amp)\\n## 3. Multi Accounts\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n On your Android device, open Chrome\\n\\n 1. Tap the ⋮ (three dots) icon to the right of the address bar\\n\\n 2. Select Clear browsing data\\n\\n 3. In the Basic settings section:\\n\\n 4. Choose a time range (default is 15 minutes)\\n\\n 5. Tap Clear data "
+  }
+}`,...(c=(i=e.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var d,u,h;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    content: "## 1. Introduction\\n1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.\\n         \\n1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.\\n\\n## 2. Your Account\\n### 2.1 Legal requirements\\n2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n\\n2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France\\n\\n![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)\\n## 3. Multi Accounts\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n"
+  }
+}`,...(h=(u=t.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};var g,l,m;n.parameters={...n.parameters,docs:{...(g=n.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: "medium",
+    content: "## 1. Introduction\\n1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.\\n         \\n1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.\\n\\n## 2. Your Account\\n### 2.1 Legal requirements\\n2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n\\n2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France\\n\\n![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)\\n## 3. Multi Accounts\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n"
+  }
+}`,...(m=(l=n.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var f,b,p;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    content: "## 1. Introduction\\n1.1. By using, visiting and/or accessing any part of the jack-poker.com website and/or registering an account on the Website, you agree to be bound by these Terms and Conditions of Service.\\n         \\n1.2. You should read the Terms carefully. If you do not agree with them, please do not use, visit, or access the Website.\\n\\n## 2. Your Account\\n### 2.1 Legal requirements\\n2.1.1 Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n\\n2.1.2 You are not allowed to register on the Website and use our services if you are a resident of Austria, Aruba, Bonaire, Curacao, France\\n\\n![welcome-offer-bg](/welcome-offer-bg-desktop.jpg)\\n## 3. Multi Accounts\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n- Reference to the “Account” shall mean an account registered by you on the Website after accepting and agreeing to these Terms.\\n"
+  }
+}`,...(p=(b=o.parameters)==null?void 0:b.docs)==null?void 0:p.source}}};var y,w,A;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    md: {
+      display: "hidden"
+    },
+    d: {
+      display: "block"
+    },
+    content: "Content is hidden in tablet screen"
+  }
+}`,...(A=(w=a.parameters)==null?void 0:w.docs)==null?void 0:A.source}}};var W,T,v;r.parameters={...r.parameters,docs:{...(W=r.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    content: '<h2 class="info-block-title">Understanding the Betting Actions in 5 Card Omaha</h2>\\n' + '<div class="info-block-description">\\n' + "<p>Getting to grips with betting actions is essential to playing poker games well. The main betting choices offered in most poker variations are summarised as follows:</p>\\n" + "<table>\\n" + "<thead>\\n" + "<tr>\\n" + "<th>Betting Option</th>\\n" + "<th>Usage</th>\\n" + "</tr>\\n" + "</thead>\\n" + "<tbody>\\n" + "<tr>\\n" + "<td>Check</td>\\n" + "<td>If there isn&rsquo;t a bet in the current betting round, we can use this option to move the action to our left without placing a wager.</td>\\n" + "</tr>\\n" + "<tr>\\n" + "<td>Bet</td>\\n" + "<td>We placed the first wager in this round of betting, compelling other players to either match it or fold.</td>\\n" + "</tr>\\n" + "<tr>\\n" + "<td>Fold</td>\\n" + "<td>We refuse to match the bet that is currently in play. Resigning from the pot and forfeiting any claim to it is known as folding.</td>\\n" + "</tr>\\n" + "<tr>\\n" + "<td>Call</td>\\n" + "<td>If there is an existing wager in the current betting round, &ldquo;calling&rdquo; means precisely matching that stake to remain in the hand.</td>\\n" + "</tr>\\n" + "<tr>\\n" + "<td>Raise</td>\\n" + "<td>We have the option of raising any wager that is already in place during the current betting round. The first bettor is then required to fold or match our raise.</td>\\n" + "</tr>\\n" + "<tr>\\n" + "<td>Re-raise</td>\\n" + "<td>We have the option of raising again if a player has already raised on the current street. &ldquo;Re-raises,&rdquo; with names like &ldquo;3-bets,&rdquo; &ldquo;4-bets,&rdquo; and so on, are raises that come after the initial one.</td>\\n" + "</tr>\\n" + "</tbody>\\n" + "</table>"
+  }
+}`,...(v=(T=r.parameters)==null?void 0:T.docs)==null?void 0:v.source}}};const L=["Primary","Large","Medium","Small","DisplayHidden","Table"];export{a as DisplayHidden,t as Large,n as Medium,e as Primary,o as Small,r as Table,L as __namedExportsOrder,F as default};
