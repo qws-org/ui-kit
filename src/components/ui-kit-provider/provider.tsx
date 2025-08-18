bf1e0933c00e3adfdf1437e05ccaae23dafa68b8
alekseishmidko <alexeishmidko@gmail.com>
2025-08-18 14:35:18 +0700

    feat:[EV-7784] обновлен uiKitProvider для переопределения стилей, убран export createCssVars

--- src/components/ui-kit-provider/provider.tsx
+++ src/components/ui-kit-provider/provider.tsx
@@ -1,12 +1,16 @@
 import type { FC, PropsWithChildren } from "react";
 
+import { createCssVars } from "~/components/ui-kit-provider/createCssVars";
+
 import { uiKitContext } from "./context";
 import type { UIKitProviderProps } from "./types";
 
 export const UIKitProvider: FC<
-  PropsWithChildren<{ value: UIKitProviderProps }>
-> = ({ value, children }) => {
+  PropsWithChildren<{ value: UIKitProviderProps; className?: string }>
+> = ({ value, children, className = "w-full h-full" }) => {
   return (
-    <uiKitContext.Provider value={value}>{children}</uiKitContext.Provider>
+    <div className={className} style={createCssVars(value.theme)}>
+      <uiKitContext.Provider value={value}>{children}</uiKitContext.Provider>
+    </div>
   );
 };
