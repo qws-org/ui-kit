e10610269cc71ccdeee656664103c6cf2626cb2c
alekseishmidko <alekseishmidko@users.noreply.github.com>
2025-09-28 10:40:37 +0000

    deploy: bda3915d70622dc7ef02ccaee83f485b7f7bb969

---
+++ assets/Checkbox-BatFqtYp.js
@@ -0,0 +1,65 @@
+import{j as e}from"./index-Ft8_MerW.js";import{useMDXComponents as m}from"./index-BqIVwv1J.js";import{M as s,C as a,a as o}from"./index-DLX3EtSJ.js";import{s as i,D as l,W as c}from"./Checkbox.stories--gVjdb2Z.js";import"./index-D4H_InIO.js";import"./index-BVDRNj0I.js";import"./iframe-Df1mjntF.js";import"./index-DPVFUrEK.js";import"./index-CpTPxDuj.js";import"./index-DrFu-skq.js";import"./Button-CG0Oxb_m.js";import"./index-DW2zCSF8.js";import"./useUiKitTheme-Du1YBrOZ.js";import"./useButton-BRi74F_i.js";import"./useFocusable-_vzmwkyz.js";import"./useFocus-DEOU5YeA.js";import"./useUiKitComponents-BuxsVqJe.js";import"./Checkbox-CjYUZjdM.js";import"./Flex-GcGlCw7p.js";import"./useControlledState-Dxxc-Ijj.js";import"./useFormValidation-lKZZF6lr.js";function t(r){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...m(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:i}),`
+`,e.jsx(n.h1,{id:"checkbox",children:"Checkbox"}),`
+`,e.jsx(n.p,{children:"Чекбокс для контроля состояния"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h2,{id:"default-code-example",children:"Default code example"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { Checkbox } from "~/components/checkbox/Checkbox";
+
+
+export const Page = () => {
+  const { handleSubmit, control } = useForm<FormValues>({
+  defaultValues: { isRemember: false },
+});
+
+const onSubmit = (data: FormValues): void => {
+  alert(JSON.stringify(data));
+};
+
+return (
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <Controller
+      control={control}
+      name="isRemember"
+      rules={{ required: false }}
+      render={({ field, fieldState: { error } }) => (
+        <Checkbox aria-label="label" errorMessage={error?.message} {...field}>
+          Remember me
+        </Checkbox>
+      )}
+    ></Controller>
+  </form>
+);
+`})}),`
+`,e.jsx(o,{of:l}),`
+`,e.jsx(n.h2,{id:"witherrormessage-code-example",children:"WithErrorMessage code example"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { Checkbox } from "~/components/checkbox/Checkbox";
+
+
+export const Page = () => {
+const { handleSubmit, control } = useForm<FormValues>({
+  defaultValues: { isRemember: false },
+});
+
+const onSubmit = (data: FormValues): void => {
+  alert(JSON.stringify(data));
+};
+
+return (
+  <form onSubmit={handleSubmit(onSubmit)}>
+     <Controller
+      control={control}
+      name="isRemember"
+      rules={{
+        required: "Вы должны согласиться с условиями!",
+      }}
+      render={({ field, fieldState: { error } }) => (
+        <Checkbox aria-label="label" errorMessage={error?.message} {...field}>
+          Remember me
+        </Checkbox>
+      )}
+    ></Controller>
+  </form>
+)
+);
+`})}),`
+`,e.jsx(o,{of:c})]})}function W(r={}){const{wrapper:n}={...m(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{W as default};
