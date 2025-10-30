762c04f18ec9d9d9ada02a712987138397f2a605
kopakama <kopakama@users.noreply.github.com>
2025-10-30 11:42:45 +0000

    deploy: 21c862df707e973c03e1745ee493f2742cccb182

---
+++ assets/Checkbox-C-Ooi-Va.js
@@ -0,0 +1,65 @@
+import{j as e}from"./jsx-runtime-BO8uF4Og.js";import{useMDXComponents as m}from"./index-BqIVwv1J.js";import{M as s,C as a,a as o}from"./index-PS14YLFZ.js";import{s as i,D as l,W as c}from"./Checkbox.stories-CJqmEaAu.js";import"./index-D4H_InIO.js";import"./iframe-mXlWz6Ae.js";import"./index-DPVFUrEK.js";import"./index-BFEZrabH.js";import"./index-BVDRNj0I.js";import"./index-CpTPxDuj.js";import"./index-DrFu-skq.js";import"./Button-CcIhuHfj.js";import"./index-DW2zCSF8.js";import"./useUiKitTheme-SvpDMYhK.js";import"./context-Cc1G11um.js";import"./useButton-YENtSbt8.js";import"./useFocus-CgYBR30T.js";import"./useFocusable-CMHRg9rf.js";import"./useUiKitComponents-DylESbeD.js";import"./Checkbox-CukaTpmg.js";import"./Flex-B_0Xiuxz.js";import"./useControlledState-Dxxc-Ijj.js";import"./useFormValidation-CRkeO8jQ.js";function t(r){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...m(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:i}),`
+`,e.jsx(n.h1,{id:"checkbox",children:"Checkbox"}),`
+`,e.jsx(n.p,{children:"Чекбокс для контроля состояния"}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h2,{id:"default-code-example",children:"Default code example"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { Checkbox } from "~/components/checkbox/Checkbox";
+
+
+export const Page = () => {
+  const { handleSubmit, control } = useForm<FormValues>({
+  defaultValues: { isRemember: false },
+});
+
+const onSubmit = (data: FormValues): void => {
+  alert(JSON.stringify(data));
+};
+
+return (
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <Controller
+      control={control}
+      name="isRemember"
+      rules={{ required: false }}
+      render={({ field, fieldState: { error } }) => (
+        <Checkbox aria-label="label" errorMessage={error?.message} {...field}>
+          Remember me
+        </Checkbox>
+      )}
+    ></Controller>
+  </form>
+);
+`})}),`
+`,e.jsx(o,{of:l}),`
+`,e.jsx(n.h2,{id:"witherrormessage-code-example",children:"WithErrorMessage code example"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { Checkbox } from "~/components/checkbox/Checkbox";
+
+
+export const Page = () => {
+const { handleSubmit, control } = useForm<FormValues>({
+  defaultValues: { isRemember: false },
+});
+
+const onSubmit = (data: FormValues): void => {
+  alert(JSON.stringify(data));
+};
+
+return (
+  <form onSubmit={handleSubmit(onSubmit)}>
+     <Controller
+      control={control}
+      name="isRemember"
+      rules={{
+        required: "Вы должны согласиться с условиями!",
+      }}
+      render={({ field, fieldState: { error } }) => (
+        <Checkbox aria-label="label" errorMessage={error?.message} {...field}>
+          Remember me
+        </Checkbox>
+      )}
+    ></Controller>
+  </form>
+)
+);
+`})}),`
+`,e.jsx(o,{of:c})]})}function q(r={}){const{wrapper:n}={...m(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(t,{...r})}):t(r)}export{q as default};
---
+++ assets/Pagination-CFLqQ3QB.js
@@ -0,0 +1,29 @@
+import{j as n}from"./jsx-runtime-BO8uF4Og.js";import{useMDXComponents as r}from"./index-BqIVwv1J.js";import{M as a,C as i,a as m}from"./index-PS14YLFZ.js";import{s,D as p}from"./Pagination.stories-CW4E4_IG.js";import"./index-D4H_InIO.js";import"./iframe-mXlWz6Ae.js";import"./index-DPVFUrEK.js";import"./index-BFEZrabH.js";import"./index-BVDRNj0I.js";import"./index-CpTPxDuj.js";import"./index-DrFu-skq.js";import"./Container-BT9Xl4e7.js";import"./Flex-B_0Xiuxz.js";import"./useUiKitTheme-SvpDMYhK.js";import"./context-Cc1G11um.js";import"./Button-CcIhuHfj.js";import"./index-DW2zCSF8.js";import"./useButton-YENtSbt8.js";import"./useFocus-CgYBR30T.js";import"./useFocusable-CMHRg9rf.js";import"./useUiKitComponents-DylESbeD.js";function o(e){const t={code:"code",h1:"h1",h3:"h3",p:"p",pre:"pre",...r(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:s}),`
+`,n.jsx(t.h1,{id:"pagination-component",children:"Pagination Component"}),`
+`,n.jsx(t.p,{children:"Компонент для отображения пагинации. Он позволяет пользователям переключаться между страницами и отображать количество элементов на каждой странице."}),`
+`,n.jsx(i,{}),`
+`,n.jsx(t.h3,{id:"default-code-example",children:"Default code example"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`import { useState } from "react";
+import { Pagination } from "./Pagination";
+import { Container } from "./Container";
+
+const Page = () => {
+  const [page, setPage] = useState(0);
+  const content = \`Контент для страницы \${page + 1}\`;
+
+  return (
+    <>
+      <Pagination
+        totalPages={5}
+        currentPage={page}
+        onPageChange={setPage}
+        itemsPerPage={10}
+      />
+      <Container justify="center" className="mt-20">
+        {content}
+      </Container>
+    </>
+  );
+};
+`})}),`
+`,n.jsx(m,{of:p})]})}function F(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{F as default};
