f0c0681acb23e13d2d3efaebf9f7fd4772432d19
rccn <rccn@deeplay.io>
2025-11-07 17:38:18 +0400

    feat: добавил поддержку кастомного триггера для popover

--- src/components/popover/PopoverTrigger.tsx
+++ src/components/popover/PopoverTrigger.tsx
@@ -6,20 +6,24 @@ import { useOverlayTriggerState } from "react-stately";
 
 import { Button } from "~/components";
 
-import { Popover } from "./Popover";
+import { Popover, type PopoverProps } from "./Popover";
 
 export interface PopoverTriggerProps extends OverlayTriggerProps {
   label: string;
   children: ReactElement;
+  customTrigger?: ReactElement;
   offset?: number;
   crossOffset?: number;
   placement?: Placement;
+  popoverProps?: Omit<PopoverProps, "triggerRef" | "state" | "children">;
 }
 
 export const PopoverTrigger: React.FC<PopoverTriggerProps> = ({
   placement = "bottom",
   label,
   children,
+  customTrigger,
+  popoverProps,
   ...props
 }) => {
   const ref = useRef(null);
@@ -31,15 +35,24 @@ export const PopoverTrigger: React.FC<PopoverTriggerProps> = ({
 
   return (
     <>
-      <Button ref={ref} {...triggerProps}>
-        {label}
-      </Button>
+      {customTrigger ? (
+        React.cloneElement(customTrigger, {
+          ref,
+          ...triggerProps,
+          onClick: triggerProps.onPress,
+        })
+      ) : (
+        <Button ref={ref} {...triggerProps}>
+          {label}
+        </Button>
+      )}
       {state.isOpen && (
         <Popover
           triggerRef={ref}
           state={state}
           placement={placement}
           {...props}
+          {...popoverProps}
         >
           {React.cloneElement(children, overlayProps)}
         </Popover>
