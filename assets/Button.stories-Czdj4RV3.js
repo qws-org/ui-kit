3c9e760324c6128658080d974f415f5b21801543
rccn-depplay <rccn-depplay@users.noreply.github.com>
2025-11-24 10:55:03 +0000

    deploy: b2f89a7171267bfb81c0ec6fbd4bd37b22324fde

---
+++ assets/Button.stories-Czdj4RV3.js
@@ -0,0 +1,206 @@
+import{j as S}from"./jsx-runtime-BO8uF4Og.js";import{fn as qr}from"./index-Cq8rKSTG.js";import{B as Ir}from"./Button-5eXfwccG.js";import"./useUiKitTheme-CDuF9_Q_.js";import{T as Er}from"./Typography-DXKVHb9x.js";import"./index-D4H_InIO.js";import"./useButton-YENtSbt8.js";import"./useFocus-CgYBR30T.js";import"./useFocusable-CMHRg9rf.js";import"./index-BFEZrabH.js";import"./index-BVDRNj0I.js";import"./useUiKitComponents-DyQDbwSn.js";const Qr={title:"UI-Kit components/Button",component:Ir,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{weight:{control:"radio",options:["superbold","bold","semibold","normal","light"]},fontWeight:{control:"radio",options:["desktop.superbold","desktop.bold","desktop.semibold","desktop.normal","desktop.light","mobile.superbold","mobile.bold","mobile.semibold","mobile.normal","mobile.light"]},lineHeight:{control:"text"}},args:{onPress:qr()}},r={args:{children:"Link",href:"/asd",elementType:"a"}},e={args:{variant:"default",children:"Default Button",p:{px:40},md:{p:{px:96},display:"inline-block"}}},a={args:{variant:"transparent",children:"Transparent Button"}},t={args:{variant:"accent",children:"White Button"}},n={args:{variant:"primary",children:"Primary Button"}},s={args:{variant:"secondary",children:"Secondary Button"}},o={args:{variant:"secondary",isActive:!0,children:"Secondary Button Active"}},i={args:{variant:"tertiary",children:"Tertiary Button"}},d={args:{size:"large",variant:"default",children:"Default Button"}},c={args:{size:"medium",variant:"default",children:"Default Button"}},l={args:{size:"small",variant:"default",children:"Default Button"}},u={args:{size:"xsmall",variant:"default",children:"Default Button"}},p={args:{size:"large",variant:"default",children:"Default Button",border:{width:"12px",color:"border-primary",radius:{topLeft:"0",topRight:"11rem",bottomLeft:"10px",bottomRight:"0"},style:"dashed"},md:{border:{style:"none",radius:{topLeft:"12px",bottomLeft:"12rem",bottomRight:"1rem"},color:"border-accent",width:"22px"}},d:{border:{style:"dashed",radius:{topLeft:"122px",bottomLeft:"12rem",bottomRight:"1rem"},color:"border-accent",width:"34px"}}}},m={args:{size:"large",variant:"default",children:"Default Button",border:{width:"1px",color:"border-primary",radius:"button"},md:{border:{style:"none",radius:{topLeft:"card",bottomLeft:"big-card",bottomRight:"checkbox",topRight:"chip"}}},d:{border:{radius:"popups"}}}},g={args:{"data-id":"data-attribute",children:"Data Attributes"}},b={render:()=>S.jsxs(Er,{as:"p",m:{mb:32},display:"inline-block",children:["Text Text Text",S.jsx(Ir,{display:"inline",href:"https://google.com",target:"_blank",m:{ml:8},variant:"tertiary",textDecoration:"underline",hover:{textDecoration:"no-underline"},color:"text-destructive",children:"qwerty"})]})},h={args:{size:"large",variant:"default",children:"Default Disabled Button",isDisabled:!0}},y={args:{size:"large",variant:"secondary",children:"secondary Disabled Button",isDisabled:!0}},f={args:{size:"large",variant:"primary",children:"primary Disabled Button",isDisabled:!0}},D={args:{size:"large",variant:"tertiary",children:"tertiary Disabled Button",isDisabled:!0}},v={args:{size:"large",variant:"accent",children:"accent Disabled Button",isDisabled:!0}},B={args:{size:"large",variant:"transparent",children:"transparent Disabled Button",isDisabled:!0}};var x,z,T;r.parameters={...r.parameters,docs:{...(x=r.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    children: "Link",
+    href: "/asd",
+    elementType: "a"
+  }
+}`,...(T=(z=r.parameters)==null?void 0:z.docs)==null?void 0:T.source}}};var A,L,k;e.parameters={...e.parameters,docs:{...(A=e.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    variant: "default",
+    children: "Default Button",
+    p: {
+      px: 40
+    },
+    md: {
+      p: {
+        px: 96
+      },
+      display: "inline-block"
+    }
+  }
+}`,...(k=(L=e.parameters)==null?void 0:L.docs)==null?void 0:k.source}}};var R,w,P;a.parameters={...a.parameters,docs:{...(R=a.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    variant: "transparent",
+    children: "Transparent Button"
+  }
+}`,...(P=(w=a.parameters)==null?void 0:w.docs)==null?void 0:P.source}}};var W,j,_;t.parameters={...t.parameters,docs:{...(W=t.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    variant: "accent",
+    children: "White Button"
+  }
+}`,...(_=(j=t.parameters)==null?void 0:j.docs)==null?void 0:_.source}}};var I,q,E;n.parameters={...n.parameters,docs:{...(I=n.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    variant: "primary",
+    children: "Primary Button"
+  }
+}`,...(E=(q=n.parameters)==null?void 0:q.docs)==null?void 0:E.source}}};var M,V,X;s.parameters={...s.parameters,docs:{...(M=s.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    children: "Secondary Button"
+  }
+}`,...(X=(V=s.parameters)==null?void 0:V.docs)==null?void 0:X.source}}};var H,K,O;o.parameters={...o.parameters,docs:{...(H=o.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    isActive: true,
+    children: "Secondary Button Active"
+  }
+}`,...(O=(K=o.parameters)==null?void 0:K.docs)==null?void 0:O.source}}};var U,C,F;i.parameters={...i.parameters,docs:{...(U=i.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    variant: "tertiary",
+    children: "Tertiary Button"
+  }
+}`,...(F=(C=i.parameters)==null?void 0:C.docs)==null?void 0:F.source}}};var G,J,N;d.parameters={...d.parameters,docs:{...(G=d.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    variant: "default",
+    children: "Default Button"
+  }
+}`,...(N=(J=d.parameters)==null?void 0:J.docs)==null?void 0:N.source}}};var Q,Y,Z;c.parameters={...c.parameters,docs:{...(Q=c.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  args: {
+    size: "medium",
+    variant: "default",
+    children: "Default Button"
+  }
+}`,...(Z=(Y=c.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var $,rr,er;l.parameters={...l.parameters,docs:{...($=l.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    variant: "default",
+    children: "Default Button"
+  }
+}`,...(er=(rr=l.parameters)==null?void 0:rr.docs)==null?void 0:er.source}}};var ar,tr,nr;u.parameters={...u.parameters,docs:{...(ar=u.parameters)==null?void 0:ar.docs,source:{originalSource:`{
+  args: {
+    size: "xsmall",
+    variant: "default",
+    children: "Default Button"
+  }
+}`,...(nr=(tr=u.parameters)==null?void 0:tr.docs)==null?void 0:nr.source}}};var sr,or,ir;p.parameters={...p.parameters,docs:{...(sr=p.parameters)==null?void 0:sr.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    variant: "default",
+    children: "Default Button",
+    border: {
+      width: "12px",
+      color: "border-primary",
+      radius: {
+        topLeft: "0",
+        topRight: "11rem",
+        bottomLeft: "10px",
+        bottomRight: "0"
+      },
+      style: "dashed"
+    },
+    md: {
+      border: {
+        style: "none",
+        radius: {
+          topLeft: "12px",
+          bottomLeft: "12rem",
+          bottomRight: "1rem"
+        },
+        color: "border-accent",
+        width: "22px"
+      }
+    },
+    d: {
+      border: {
+        style: "dashed",
+        radius: {
+          topLeft: "122px",
+          bottomLeft: "12rem",
+          bottomRight: "1rem"
+        },
+        color: "border-accent",
+        width: "34px"
+      }
+    }
+  }
+}`,...(ir=(or=p.parameters)==null?void 0:or.docs)==null?void 0:ir.source}}};var dr,cr,lr;m.parameters={...m.parameters,docs:{...(dr=m.parameters)==null?void 0:dr.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    variant: "default",
+    children: "Default Button",
+    border: {
+      width: "1px",
+      color: "border-primary",
+      radius: "button"
+    },
+    md: {
+      border: {
+        style: "none",
+        radius: {
+          topLeft: "card",
+          bottomLeft: "big-card",
+          bottomRight: "checkbox",
+          topRight: "chip"
+        }
+      }
+    },
+    d: {
+      border: {
+        radius: "popups"
+      }
+    }
+  }
+}`,...(lr=(cr=m.parameters)==null?void 0:cr.docs)==null?void 0:lr.source}}};var ur,pr,mr;g.parameters={...g.parameters,docs:{...(ur=g.parameters)==null?void 0:ur.docs,source:{originalSource:`{
+  args: {
+    "data-id": "data-attribute",
+    children: "Data Attributes"
+  }
+}`,...(mr=(pr=g.parameters)==null?void 0:pr.docs)==null?void 0:mr.source}}};var gr,br,hr;b.parameters={...b.parameters,docs:{...(gr=b.parameters)==null?void 0:gr.docs,source:{originalSource:`{
+  render: () => {
+    return <Typography as={"p"} m={{
+      mb: 32
+    }} display="inline-block">
+        Text Text Text
+        <Button display="inline" href={"https://google.com"} target="_blank" m={{
+        ml: 8
+      }} variant={"tertiary"} textDecoration={"underline"} hover={{
+        textDecoration: "no-underline"
+      }} color={"text-destructive"}>
+          qwerty
+        </Button>
+      </Typography>;
+  }
+}`,...(hr=(br=b.parameters)==null?void 0:br.docs)==null?void 0:hr.source}}};var yr,fr,Dr;h.parameters={...h.parameters,docs:{...(yr=h.parameters)==null?void 0:yr.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    variant: "default",
+    children: "Default Disabled Button",
+    isDisabled: true
+  }
+}`,...(Dr=(fr=h.parameters)==null?void 0:fr.docs)==null?void 0:Dr.source}}};var vr,Br,Sr;y.parameters={...y.parameters,docs:{...(vr=y.parameters)==null?void 0:vr.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    variant: "secondary",
+    children: "secondary Disabled Button",
+    isDisabled: true
+  }
+}`,...(Sr=(Br=y.parameters)==null?void 0:Br.docs)==null?void 0:Sr.source}}};var xr,zr,Tr;f.parameters={...f.parameters,docs:{...(xr=f.parameters)==null?void 0:xr.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    variant: "primary",
+    children: "primary Disabled Button",
+    isDisabled: true
+  }
+}`,...(Tr=(zr=f.parameters)==null?void 0:zr.docs)==null?void 0:Tr.source}}};var Ar,Lr,kr;D.parameters={...D.parameters,docs:{...(Ar=D.parameters)==null?void 0:Ar.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    variant: "tertiary",
+    children: "tertiary Disabled Button",
+    isDisabled: true
+  }
+}`,...(kr=(Lr=D.parameters)==null?void 0:Lr.docs)==null?void 0:kr.source}}};var Rr,wr,Pr;v.parameters={...v.parameters,docs:{...(Rr=v.parameters)==null?void 0:Rr.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    variant: "accent",
+    children: "accent Disabled Button",
+    isDisabled: true
+  }
+}`,...(Pr=(wr=v.parameters)==null?void 0:wr.docs)==null?void 0:Pr.source}}};var Wr,jr,_r;B.parameters={...B.parameters,docs:{...(Wr=B.parameters)==null?void 0:Wr.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    variant: "transparent",
+    children: "transparent Disabled Button",
+    isDisabled: true
+  }
+}`,...(_r=(jr=B.parameters)==null?void 0:jr.docs)==null?void 0:_r.source}}};const Yr=["Link","Default","Transparent","Accent","Primary","Secondary","SecondaryActive","Tertiary","SizeLarge","SizeMedium","SizeSmall","SizeXsmall","Border","BorderWithTokenValues","DataAttributes","InlineBlock","DefaultAndDisabled","SecondaryAndDisabled","PrimaryAndDisabled","TertiaryAndDisabled","AccentAndDisabled","TransparentAndDisabled"];export{t as Accent,v as AccentAndDisabled,p as Border,m as BorderWithTokenValues,g as DataAttributes,e as Default,h as DefaultAndDisabled,b as InlineBlock,r as Link,n as Primary,f as PrimaryAndDisabled,s as Secondary,o as SecondaryActive,y as SecondaryAndDisabled,d as SizeLarge,c as SizeMedium,l as SizeSmall,u as SizeXsmall,i as Tertiary,D as TertiaryAndDisabled,a as Transparent,B as TransparentAndDisabled,Yr as __namedExportsOrder,Qr as default};
