3c22aa5f139d987349ea7d3a47a333ef3d03a9e9
rccn-depplay <rccn-depplay@users.noreply.github.com>
2025-11-27 10:22:39 +0000

    deploy: f7ad38413e54109087056ab6724964b55a7f4b58

---
+++ assets/Pagination-CVdSD2qT.js
@@ -0,0 +1,29 @@
+import{j as n}from"./jsx-runtime-BO8uF4Og.js";import{useMDXComponents as r}from"./index-BqIVwv1J.js";import{M as a,C as i,a as s}from"./index-D1AKDNRi.js";import{s as m,D as p}from"./Pagination.stories-Pvll2gT2.js";import"./index-D4H_InIO.js";import"./iframe-huwgEaU5.js";import"./index-DPVFUrEK.js";import"./index-BFEZrabH.js";import"./index-BVDRNj0I.js";import"./index-CpTPxDuj.js";import"./index-DrFu-skq.js";import"./Container-B0H4pMat.js";import"./Flex-D2E7PNVu.js";import"./useUiKitTheme-DTw4gFRC.js";import"./Button-BpBnmJK5.js";import"./useButton-YENtSbt8.js";import"./useFocus-CgYBR30T.js";import"./useFocusable-CMHRg9rf.js";import"./useUiKitComponents-BxXGiyE_.js";function o(e){const t={code:"code",h1:"h1",h3:"h3",p:"p",pre:"pre",...r(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:m}),`
+`,n.jsx(t.h1,{id:"pagination-component",children:"Pagination Component"}),`
+`,n.jsx(t.p,{children:"Компонент для отображения пагинации. Он позволяет пользователям переключаться между страницами и отображать количество элементов на каждой странице."}),`
+`,n.jsx(i,{}),`
+`,n.jsx(t.h3,{id:"default-code-example",children:"Default code example"}),`
+`,n.jsx(t.pre,{children:n.jsx(t.code,{className:"language-tsx",children:`import { useState } from "react";
+import { Pagination } from "./Pagination";
+import { Container } from "./Container";
+
+const Page = () => {
+  const [page, setPage] = useState(0);
+  const content = \`Контент для страницы \${page + 1}\`;
+
+  return (
+    <>
+      <Pagination
+        totalPages={5}
+        currentPage={page}
+        onPageChange={setPage}
+        itemsPerPage={10}
+      />
+      <Container justify="center" className="mt-20">
+        {content}
+      </Container>
+    </>
+  );
+};
+`})}),`
+`,n.jsx(s,{of:p})]})}function w(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{w as default};
