162b8c6aaa7bc940da93cb3a44f7aae79f5ea4c7
rccn-depplay <rccn-depplay@users.noreply.github.com>
2025-12-22 10:37:04 +0000

    deploy: 556ee69eedf0d30c78e7792debd01a9abe178c14

---
+++ assets/Card.stories-CeWcFJR8.js
@@ -0,0 +1,45 @@
+import{j as c}from"./jsx-runtime-BO8uF4Og.js";import{C as V}from"./useUiKitTheme-Cs989wUB.js";import"./index-D4H_InIO.js";const w={title:"UI-Kit components/Card",component:V,tags:["autodocs"],argTypes:{className:{control:{type:"text"},description:"Дополнительные CSS-классы для карточки"},children:{control:{type:"text"},description:"Содержимое карточки"}}},r={args:{children:"Это карточка по умолчанию"}},a={args:{className:"bg-blue-500 text-white",children:"Карточка с пользовательскими классами",p:40,m:32}},e={args:{children:c.jsxs("div",{className:"p-4",children:[c.jsx("h3",{className:"text-lg font-bold",children:"Заголовок"}),c.jsx("p",{className:"text-sm",children:"Это пример вложенного содержимого в карточке."})]})}},s={args:{variant:"secondary",children:"Это карточка с вариантом secondary,"}},t={args:{variant:"tertiary",children:"Это карточка с вариантом tertiary,"}},n={args:{variant:"default",bg:"background-accent2",md:{bg:"background-tetriary"},d:{bg:"background-accent1-vertical"},border:{radius:"40px"},className:"rounded md:rounded-[33px] h-[300px]",children:"Это карточка с вариантом default, и добавлен bg для кастомизации цвета подложки и border-radius, в приоритете пропсы, те параметры что необходимо дописать можно дополнить через className "}};var o,d,i;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: "Это карточка по умолчанию"
+  }
+}`,...(i=(d=r.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};var m,l,p;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    className: "bg-blue-500 text-white",
+    children: "Карточка с пользовательскими классами",
+    p: 40,
+    m: 32
+  }
+}`,...(p=(l=a.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var u,g,h;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    children: <div className="p-4">
+        <h3 className="text-lg font-bold">Заголовок</h3>
+        <p className="text-sm">Это пример вложенного содержимого в карточке.</p>
+      </div>
+  }
+}`,...(h=(g=e.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var b,x,y;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    children: "Это карточка с вариантом secondary,"
+  }
+}`,...(y=(x=s.parameters)==null?void 0:x.docs)==null?void 0:y.source}}};var N,C,f;t.parameters={...t.parameters,docs:{...(N=t.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    variant: "tertiary",
+    children: "Это карточка с вариантом tertiary,"
+  }
+}`,...(f=(C=t.parameters)==null?void 0:C.docs)==null?void 0:f.source}}};var v,S,k;n.parameters={...n.parameters,docs:{...(v=n.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    variant: "default",
+    bg: "background-accent2",
+    md: {
+      bg: "background-tetriary"
+    },
+    d: {
+      bg: "background-accent1-vertical"
+    },
+    border: {
+      radius: "40px"
+    },
+    className: "rounded md:rounded-[33px] h-[300px]",
+    children: "Это карточка с вариантом default, и добавлен bg для кастомизации цвета подложки и border-radius," + " в приоритете пропсы, те параметры что необходимо дописать можно дополнить через className "
+  }
+}`,...(k=(S=n.parameters)==null?void 0:S.docs)==null?void 0:k.source}}};const B=["Default","WithCustomClass","WithNestedContent","SecondaryVariant","TertiaryVariant","CustomVariantWithCustomBg"];export{n as CustomVariantWithCustomBg,r as Default,s as SecondaryVariant,t as TertiaryVariant,a as WithCustomClass,e as WithNestedContent,B as __namedExportsOrder,w as default};
