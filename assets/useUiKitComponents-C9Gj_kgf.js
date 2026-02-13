49a9b57611ff596fa59d6258d303cef3567772a2
kopakama <kopakama@users.noreply.github.com>
2026-02-13 07:11:21 +0000

    deploy: c4e5b31e08121129201f9742a0bf06bf43640786

---
+++ assets/useUiKitComponents-C9Gj_kgf.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-D4H_InIO.js";import{u as r}from"./useUiKitTheme-DbHycIcO.js";const i=()=>{const o=t.useContext(r);if(o===null)throw new Error("data of UIKitProvider cannot be null, please add 'UIKitProvider' to the root of your application");return o.components};export{i as u};
