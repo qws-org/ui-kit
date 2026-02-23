c3629f455340749513d1b979cdf206074ac49612
rccn-depplay <rccn@deeplay.io>
2026-02-23 16:31:27 +0400

    feat: scroll test story (#99)
    Co-authored-by: Andrei S <andrey_overdose@mail.ru>

--- src/components/modal/Modal.stories.tsx
+++ src/components/modal/Modal.stories.tsx
@@ -374,3 +374,219 @@ export const ModalWithCustomRoot: StoryObj = {
     </ModalTrigger>
   ),
 };
+
+export const ModalFix: StoryObj = {
+  render: () => (
+    <ModalTrigger
+      label="Click"
+      portalContainer={document.getElementById("storybook-root") ?? undefined}
+      height="100%"
+    >
+      {(close) => (
+        <Flex
+          height="var(--app-height)"
+          flexDirection="column"
+          border={{ radius: "0" }}
+          width="100%"
+          md={{
+            width: "660px",
+            height: "760px",
+            maxHeight: "96vh",
+            border: { radius: "16px" },
+          }}
+          data-testid="promo-modal"
+        >
+          <Flex
+            align="center"
+            position={{ type: "relative" }}
+            gap={15}
+            p={{ pl: 16 }}
+            md={{ p: { pl: 32 }, border: { radius: "16px" } }}
+            border={{ radius: "0 0 16px 16px" }}
+            style={{ flexShrink: 0 }}
+            overflow="hidden"
+          >
+            <Flex flexDirection="column" gap={8} width="100%">
+              <Typography
+                fontWeight="mobile.bold"
+                fontFamily="accent"
+                data-testid="modal-title"
+              >
+                Title
+              </Typography>
+              <Flex flexDirection="column">Subtitle</Flex>
+            </Flex>
+            <Flex
+              style={{ flexShrink: 0 }}
+              m={{ ml: "auto" }}
+              md={{ width: 238, height: 210 }}
+              width={161}
+              height={142}
+            >
+              <img
+                src="https://placehold.co/600x400"
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </Flex>
+            <Flex
+              backdropBlur={12}
+              position={{ type: "absolute", top: 6, right: 6 }}
+              md={{ position: { top: 16, right: 16 } }}
+              border={{ radius: "8px" }}
+            >
+              <Button
+                p={10}
+                variant="secondary"
+                md={{ p: 14 }}
+                onPress={close}
+                data-testid="modal-close-button"
+              >
+                <Close width={20} height={20} />
+              </Button>
+            </Flex>
+          </Flex>
+          <Flex
+            flexDirection="column"
+            gap={16}
+            p={16}
+            md={{
+              gap: 24,
+              p: 32,
+            }}
+            position={{ type: "relative" }}
+            overflow="auto"
+            style={{
+              WebkitOverflowScrolling: "touch",
+              flex: "1 1 auto",
+            }}
+          >
+            <Flex flexDirection="column" gap={16}>
+              <Typography
+                fontWeight="mobile.bold"
+                fontFamily="accent"
+                data-testid="modal-description-title"
+                style={{ wordBreak: "break-word" }}
+                whiteSpace="pre-wrap"
+              >
+                Description Title lorem Description Title lorem Description
+                Title lorem Description Title loremDescription Title lorem
+                Description Title lorem Description Title lorem Description
+                Title lorem Description Title lorem Description Title lorem
+                Description Title lorem Description Title lorem Description
+                Title lorem
+              </Typography>
+              <Typography
+                fontFamily="accent"
+                fontSize="body.mobile.xsmall"
+                lineHeight="body.mobile.xsmall"
+                md={{
+                  fontSize: "body.desktop.medium",
+                  lineHeight: "body.desktop.medium",
+                }}
+                data-testid="modal-description"
+                style={{ wordBreak: "break-word" }}
+                whiteSpace="pre-wrap"
+              >
+                Title lorem Description Title lorem Description Title lorem
+                Description Title lorem Description Title lorem Description
+                Title lorem
+              </Typography>
+            </Flex>
+            <Flex flexDirection="column" gap={16}>
+              <Typography
+                fontWeight="mobile.bold"
+                fontFamily="accent"
+                fontSize="title.mobile.xsmall"
+                lineHeight="title.mobile.xsmall"
+                data-testid="modal-terms-title"
+              >
+                Terms
+              </Typography>
+              {[
+                "Term 1",
+                "Term 2",
+                "Term 3",
+                "Term 4",
+                "Term 5",
+                "Term 6",
+                "Term 7",
+                "Term 8",
+                "Term 9",
+                "Term 10",
+              ].map((el, i) => (
+                <Flex key={i} align="start" gap={12}>
+                  <Flex
+                    align="center"
+                    justify="center"
+                    bg="surface-neutral-default"
+                    border={{ radius: "50%" }}
+                    width={24}
+                    height={24}
+                    style={{ flexShrink: 0 }}
+                    data-testid={`modal-term-${i}`}
+                  >
+                    <Typography
+                      color="text-primary-default"
+                      fontWeight="mobile.bold"
+                      fontFamily="accent"
+                      fontSize="body.mobile.xsmall"
+                      lineHeight="body.mobile.xsmall"
+                    >
+                      {i + 1}
+                    </Typography>
+                  </Flex>
+                  <Typography
+                    color="text-primary-muted"
+                    fontFamily="accent"
+                    md={{
+                      fontSize: "body.desktop.medium",
+                      lineHeight: "body.desktop.medium",
+                    }}
+                    fontSize="body.mobile.xsmall"
+                    lineHeight="body.mobile.xsmall"
+                    data-testid={`modal-term-${i}`}
+                    style={{ wordBreak: "break-word" }}
+                    whiteSpace="pre-wrap"
+                  >
+                    {el}
+                  </Typography>
+                </Flex>
+              ))}
+            </Flex>
+          </Flex>
+          <Flex
+            p={16}
+            bg="background-popups2"
+            width="100%"
+            border={{ radius: "16px 16px 0 0" }}
+            md={{ border: { radius: "16px" } }}
+            style={{ flexShrink: 0 }}
+          >
+            <Button
+              bg="surface-accent-button-default"
+              border={{ radius: "8px" }}
+              width="100%"
+              p={{ px: 20, py: 14 }}
+              onPress={close}
+              data-testid="modal-action-button"
+            >
+              <Typography
+                color="text-primary-inverted"
+                fontWeight="mobile.bold"
+                fontFamily="accent"
+                fontSize="body.mobile.xsmall"
+                lineHeight="body.mobile.xsmall"
+                md={{
+                  fontSize: "body.desktop.medium",
+                  lineHeight: "body.desktop.medium",
+                }}
+              >
+                Register
+              </Typography>
+            </Button>
+          </Flex>
+        </Flex>
+      )}
+    </ModalTrigger>
+  ),
+};
